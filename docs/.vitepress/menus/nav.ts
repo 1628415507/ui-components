b26a74c974e09eb3853b1707f76b562ec5fb8520
blank <1628415507@qq.com>
2024-10-20 21:27:39 +0800

    feat:专栏-计算机网络-cookie

--- docs/.vitepress/menus/nav.ts
+++ docs/.vitepress/menus/nav.ts
@@ -4,7 +4,7 @@
  * @LastEditTime: 2024-10-18 11:14:36
  */
 //
-import { DefaultTheme } from 'vitepress';
+import { DefaultTheme } from 'vitepress'
 // 博客目录参考： https://www.kancloud.cn/surahe/front-end-notebook/482370
 // 顶部菜单
 export const nav: DefaultTheme.NavItem[] = [
@@ -27,7 +27,7 @@ export const nav: DefaultTheme.NavItem[] = [
   {
     text: '专栏',
     items: [
-       // {
+      // {
       //   text: 'HTML',
       //   link: '/blogs/css' // 对应docs/column/Algorithm下的index.md文件
       // },
@@ -39,23 +39,23 @@ export const nav: DefaultTheme.NavItem[] = [
         text: 'JavaScript',
         link: '/blogs/javaScript' // 对应docs/column/Algorithm下的index.md文件
       },
-       {
-        text: '前端框架',//算法。浏览器‘正则表达式
+      {
+        text: '计算机网络',
+        link: '/blogs/network' // 对应docs/column/Algorithm下的index.md文件
+      },
+      {
+        text: '前端框架', //算法。浏览器‘正则表达式
         link: '/blogs/framework' // 对应docs/column/Algorithm下的index.md文件
       },
-      // {
-      //   text: '服务端',//算法。浏览器‘正则表达式
-      //   link: '/examples' // 对应docs/column/Algorithm下的index.md文件
-      // },
       {
         text: '业务场景',
         link: '/blogs/business' // 对应docs/column/Algorithm下的index.md文件
-      },
+      }
       // {
       //   text: '方法',
       //   link: '/examples' // 对应docs/column/Algorithm下的index.md文件
       // }
     ]
-  },
+  }
   // { text: '组件', link: '/markdown-examples' }
 ]
---
+++ docs/.vitepress/menus/sidebar/network.ts
@@ -0,0 +1,4 @@
+export default [
+  //
+  { text: '浏览器缓存', link: '/blogs/browser/storage.md' }
+]
