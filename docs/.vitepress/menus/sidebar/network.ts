dfeeecc2cb740d5db9220cfd044a7621cae57752
blank <1628415507@qq.com>
2024-10-21 22:04:34 +0800

    feat:专栏-计算机网络-DNS 协议了解多少

--- docs/.vitepress/menus/sidebar/network.ts
+++ docs/.vitepress/menus/sidebar/network.ts
@@ -1,5 +1,5 @@
 export default [
   //
-  { text: '汇总', link: '/blogs/browser/index.md' },
+  { text: '网络', link: '/blogs/browser/index.md' },
   { text: '浏览器缓存', link: '/blogs/browser/storage.md' }
 ]
