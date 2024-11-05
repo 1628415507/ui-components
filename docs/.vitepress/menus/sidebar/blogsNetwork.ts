1402f12ec856eabf9d1fa69213914f1d72d9b202
hongzf <1628415507@qq.com>
2024-11-05 15:00:41 +0800

    blogs：专栏-浏览器-浏览器对队头阻塞有什么优化？

--- docs/.vitepress/menus/sidebar/blogsNetwork.ts
+++ docs/.vitepress/menus/sidebar/blogsNetwork.ts
@@ -1,11 +1,12 @@
 /*
  * @Description: 
  * @Date: 2024-10-30 18:10:00
- * @LastEditTime: 2024-11-05 13:47:14
+ * @LastEditTime: 2024-11-05 14:24:37
  */
 export default [
   //
   { text: '网络', link: '/blogs/network/index.md' },
   { text: '浏览器', link: '/blogs/network/browser.md' },
-  { text: '浏览器缓存', link: '/blogs/network/storage.md' }
+  { text: '浏览器缓存', link: '/blogs/network/storage.md' },
+  { text: 'HTTP 和 HTTPS', link: '/blogs/network/https.md' }
 ]
