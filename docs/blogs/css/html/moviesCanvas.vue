6ec27d22118093e218ad48c97bf4bb12f84b9a43
blank <1628415507@qq.com>
2024-12-01 22:04:32 +0800

    blogs：专栏Html&css笔记调整

--- docs/blogs/css/html/moviesCanvas.vue
+++ docs/blogs/css/html/moviesCanvas.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <el-button @click="initSeat()" class="mb-05">重置</el-button>
     <el-button id="btnPay" @click="handlePay" class="mb-05">确认并⽀付</el-button>
     <div style="color: red">
       {{ tip }}
@@ -13,18 +14,18 @@
 <script setup>
 import { onMounted, ref } from 'vue'
 // 获取画布和按钮元素
-let canvas //= document.getElementById('canvas')
-let btnPay // = document.getElementById('btnPay')
+let canvas
+let btnPay
 const width = 30
 const gap = width + 20 // 间距20
-const tip = ref('') // 间距20
+const tip = ref('')
 
 // 初始化座位
 onMounted(() => {
   canvas = document.getElementById('canvasMovies')
   btnPay = document.getElementById('btnPay')
   initSeat()
-  canvas.addEventListener('click', handleClick) // 绑定点击事件和确认并⽀付按钮点击事件
+  canvas.addEventListener('click', clickSeat) // 绑定点击事件和确认并⽀付按钮点击事件
 })
 let seats = []
 // 绘制座位
@@ -35,7 +36,7 @@ function drawSeat(x, y, state) {
       ctx.fillStyle = '#ccc' // 可选座位
       break
     case 'sold':
-      ctx.fillStyle = '#f00' // 已售座位
+      ctx.fillStyle = 'pink' // 已售座位
       break
     case 'selected':
       ctx.fillStyle = '#42b983' // 已选座位
@@ -71,37 +72,24 @@ function countSelectedSeats() {
   return [count, selectedSeats]
 }
 // 更新座位状态和颜⾊
-function updateSeat(x, y) {
-  if (seats[x][y] == 'selectable') {
-    seats[x][y] = 'selected' // 更改为已选状态
-  } else if (seats[x][y] == 'selected') {
-    seats[x][y] = 'selectable' // 更改为可选状态
-  }
+function updateSeat(x, y, state) {
+  seats[x][y] = state // 更改为已选状态
   drawSeat(x, y, seats[x][y]) // 更新颜⾊
 }
-// 检查座位状态是否可选
-function checkSeat(x, y) {
-  if (seats[x][y] == 'sold') {
-    tip.value = '该座位已售出，请选择其他座位！'
-    return false
-  } else if (seats[x][y] == 'selected') {
-    tip.value = '该座位已被选中，请选择其他座位！'
-    return false
-  }
-  return true
-}
 // 点击事件处理函数
-function handleClick(e) {
-  console.log('【 handleClick(e 】-90', e)
+function clickSeat(e) {
+  // e.offsetX:（鼠标位置）相对于最近父级元素的坐标
+  // el.offsetLeft: 元素相对于最近定位父级元素的坐标,当前示例中为0
   let diffX = e.offsetX - canvas.offsetLeft < 0 ? 0 : e.offsetX - canvas.offsetLeft
+  // console.log('【 e.offsetX - canvas.offsetLeft 】-97', e.offsetX, canvas.offsetLeft)
   let diffY = e.offsetY - canvas.offsetTop < 0 ? 0 : e.offsetY - canvas.offsetTop
   // console.log('【 diffX 】-94', diffX, diffY, e.offsetY, canvas.offsetTop)
   let x = parseInt(diffX / gap)
   let y = parseInt(diffY / gap)
   console.log('【 坐标 】-99', x, y)
   if (x >= 0 && x < 10 && y >= 0 && y < 10) {
-    if (checkSeat(x, y)) {
-      updateSeat(x, y)
+    if (checkSelectable(x, y)) {
+      updateSeat(x, y, 'selected')
       let count = countSelectedSeats()[0]
       if (count > 0) {
         btnPay.innerHTML = '确认并⽀付（已选 ' + count + ' 座位）'
@@ -111,10 +99,21 @@ function handleClick(e) {
     }
   }
 }
+// 检查座位状态是否可选
+function checkSelectable(x, y) {
+  if (seats[x][y] == 'sold') {
+    tip.value = '该座位已售出，请选择其他座位！'
+    return false
+  } else if (seats[x][y] == 'selected') {
+    tip.value = '该座位已被选中，请选择其他座位！'
+    return false
+  }
+  return true
+}
+
 // 确认并⽀付按钮点击事件处理函数
 function handlePay() {
   let selectedSeats = countSelectedSeats()[1]
-  console.log('【 selectedSeats 】-99', selectedSeats)
   if (selectedSeats.length == 0) {
     tip.value = '请选择座位！'
     return
@@ -122,8 +121,10 @@ function handlePay() {
   if (confirm('您已选中以下座位：' + selectedSeats.join('、') + '，确认⽀付吗？')) {
     // 向后台发送选票信息，并进⾏⽀付处理
     tip.value = '⽀付成功！请前往指定影院取票！'
-    initSeat() // 重新初始化座位
     btnPay.innerHTML = '确认并⽀付'
+    for (let item of selectedSeats) {
+      updateSeat(item[0], item[1], 'sold')
+    }
   }
 }
 </script>
