c8e71f1043d48d1f2aef77813e443f12a949027b
blank <1628415507@qq.com>
2025-01-07 23:44:25 +0800

    feat: vue 的双向绑定的原理

--- docs/.vitepress/menus/sidebar/blogsFramework.ts
+++ docs/.vitepress/menus/sidebar/blogsFramework.ts
@@ -1,5 +1,5 @@
 /*
- * @Description: 
+ * @Description:
  * @Date: 2024-10-14 17:14:53
  * @LastEditTime: 2025-01-07 17:42:55
  */
@@ -10,10 +10,11 @@ export default [
     items: [
       { text: 'vue', link: '/blogs/framework/vue/index.md' },
       { text: '虚拟DOM与Diff算法分析', link: '/blogs/framework/vue/diff/index.md' },
-      { text: 'MVC与MVVM模式的区别', link: '/blogs/framework/vue/mvvm.md' },
+      { text: 'vue 的双向绑定的原理', link: '/blogs/framework/vue/defineProperty.md' },
+      { text: 'MVC与MVVM模式的区别', link: '/blogs/framework/vue/mvvm.md' }
     ]
   },
   { text: 'TypeScript入门', link: '/blogs/framework/typeScript/index.md' },
   { text: 'Cli相关', link: '/blogs/framework/cli/index.md' },
-  { text: 'Webpack', link: '/blogs/framework/cli/webpack.md' },
+  { text: 'Webpack', link: '/blogs/framework/cli/webpack.md' }
 ]
