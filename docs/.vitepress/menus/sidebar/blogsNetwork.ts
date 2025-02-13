e0d3ad2335476544a4afc9ffee2a99a248e90318
blank <1628415507@qq.com>
2025-02-13 23:35:26 +0800

    docs：浏览器-从浏览器输入 url 后都经历了什么

--- docs/.vitepress/menus/sidebar/blogsNetwork.ts
+++ docs/.vitepress/menus/sidebar/blogsNetwork.ts
@@ -10,6 +10,7 @@ export default [
   { text: '浏览器数据存储', link: '/blogs/network/storage.md' },
   { text: '浏览器缓存机制', link: '/blogs/network/cache.md' },
   { text: 'TCP', link: '/blogs/network/tcp.md' },
+  { text: '从浏览器输入 url 后都经历了什么', link: '/blogs/network/url.md' },
   { text: 'HTTP 和 HTTPS', link: '/blogs/network/https.md' },
   { text: 'XSS 和 CSRF', link: '/blogs/network/xss&csrf.md' }
 ]
