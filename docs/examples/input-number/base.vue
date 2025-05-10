9908c20551a35e3eef0fa3c425861c17b50b8b5c
hongzf <1628415507@qq.com>
2025-05-10 17:28:13 +0800

    fix: 打包配置调整-整体引入示例测试

--- docs/examples/input-number/base.vue
+++ docs/examples/input-number/base.vue
@@ -1,7 +1,7 @@
 <!--
  * @Description: 
  * @Date: 2024-08-19 11:19:22
- * @LastEditTime: 2025-05-10 13:57:30
+ * @LastEditTime: 2025-05-10 17:17:17
 -->
 <template>
   <div>
@@ -10,7 +10,8 @@
 </template>
 <script setup lang="ts">
 import { onMounted, Ref, ref } from 'vue'
-const inputRef = ref()
+import type { InputNumberInstance } from '../../../src/index.ts'
+const inputRef = ref<InputNumberInstance>()
 onMounted(() => {
   console.log('【 inputRef 】-16', inputRef.value)
 })
