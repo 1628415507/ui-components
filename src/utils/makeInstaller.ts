95452e41c609c7c876fdcbdc731f2e5b4d70a8ad
hongzf <1628415507@qq.com>
2025-05-10 14:18:01 +0800

    feat:package文件说明

--- src/utils/makeInstaller.ts
+++ src/utils/makeInstaller.ts
@@ -1,11 +1,13 @@
-// makeInstaller 实际上也是一个vue插件，他将组件插件循环进行安装，也是从element-plus复制的
+// makeInstaller 实际上也是一个vue插件，
+// 他将组件插件循环进行安装，也是从element - plus复制的
 import type { App, Plugin } from 'vue';
 export const makeInstaller = (components: Plugin[] = []) => {
   const install = (app: App) => {
-    console.log(components);
+    // 将组件插件循环进行安装
     components.forEach((c) => app.use(c));
   };
   return {
-    install,
+    install,//返回一个install方法，将组件插件循环进行安装(
+    // 作用: 处理成一个支持整体导入的插件)
   };
 };
