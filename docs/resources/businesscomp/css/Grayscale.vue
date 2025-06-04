2ee5bcffa46d0ab7ed2705a387308fd7cfbea79d
hongzf <1628415507@qq.com>
2025-06-04 13:39:18 +0800

    feat:黑白网页效果

---
+++ docs/resources/businesscomp/css/Grayscale.vue
@@ -0,0 +1,25 @@
+<!--
+ * @Description: 
+ * @Date: 2024-10-31 15:18:02
+ * @LastEditTime: 2025-06-04 13:35:59
+-->
+<template>
+  <el-button type="primary" @click="setGrayscaleStyle(!isGrayscale)">黑白：{{ isGrayscale }}</el-button>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+const isGrayscale: boolean = ref(false)
+// 设置黑白样式到html上
+function setGrayscaleStyle(val: boolean) {
+  isGrayscale.value = val
+  let grayscale: string = isGrayscale.value ? 'grayscale(100%)' : ''
+  document.documentElement.style.setProperty('filter', grayscale)
+  document.documentElement.style.setProperty('-webkit-filter', grayscale)
+  document.documentElement.style.setProperty('-moz-filter', grayscale)
+  document.documentElement.style.setProperty('-ms-filter', grayscale)
+  document.documentElement.style.setProperty('-o-filter', grayscale)
+  // document.documentElement.classList.add('grayscale')//class没效果？
+  // document.documentElement.classList.remove('grayscale')
+}
+</script>
--- docs/resources/businesscomp/css/index.md
+++ docs/resources/businesscomp/css/index.md
@@ -1,10 +1,17 @@
 <!--
- * @Description: 
+ * @Description:
  * @Date: 2024-10-30 16:58:47
- * @LastEditTime: 2024-10-31 15:21:05
+ * @LastEditTime: 2025-06-04 13:37:47
 -->
 
-##  树添加指示线线
+## 树添加指示线
+
 ::: example
 resources/businesscomp/css/LineTree
+:::
+
+## 黑白网页效果
+
+::: example
+resources/businesscomp/css/Grayscale
 :::
